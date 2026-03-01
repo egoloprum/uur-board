2e918eae0511c42297eb9516fae3c983f2f628f8
egoloprum <bayraa093@gmail.com>
2026-03-01 23:33:25 +0300

    fix: season refactoring

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,3 +1,43 @@
+import { Stick } from '@/src/shared/components'
+import { colorMap, fontMap, getRotationDegree, getRotationStyle } from '@/src/shared/utils'
+import Link from 'next/link'
+
 export const Header = ({}) => {
-  return <header className="bg-white p-4 text-black">Header</header>
+  const randomFont = fontMap[Math.floor(Math.random() * fontMap.length)]
+  const randomBgColor = colorMap[Math.floor(Math.random() * colorMap.length)]
+
+  const rotationDegree = getRotationDegree()
+  const rotationStyle = getRotationStyle(rotationDegree)
+
+  const leftStickRotation = rotationDegree + 25
+  const rightStickRotation = rotationDegree - 25
+
+  return (
+    <header className="px-4 py-4 md:px-8 md:py-6 lg:px-12 lg:py-8 text-black flex justify-between overflow-hidden">
+      <div className="relative">
+        <Stick position="left" rotation={leftStickRotation} />
+        <Link href="/">
+          <nav
+            className={`h-fit w-fit text-black p-4 py-2 text-xl sm:text-4xl md:text-5xl lg:text-6xl shadow-2xl hover:scale-110 transition-all duration-300 ${randomFont} ${randomBgColor}`}
+            style={{ ...rotationStyle }}
+          >
+            <p>ҮҮР</p>
+          </nav>
+        </Link>
+        <Stick position="right" rotation={rightStickRotation} />
+      </div>
+      <div className="relative">
+        <Stick position="left" rotation={leftStickRotation} />
+        <Link href="/about">
+          <nav
+            className={`h-fit w-fit text-black p-4 py-2 text-xl sm:text-2xl md:text-3xl lg:text-4xl shadow-2xl hover:scale-110 transition-all duration-300 ${randomFont} ${randomBgColor}`}
+            style={{ ...rotationStyle }}
+          >
+            <p>Бидний тухай</p>
+          </nav>
+        </Link>
+        <Stick position="right" rotation={rightStickRotation} />
+      </div>
+    </header>
+  )
 }
--- src/widgets/landing/season/SeasonWidget.tsx
+++ src/widgets/landing/season/SeasonWidget.tsx
@@ -1,10 +1,4 @@
-import {
-  colorMap,
-  fontMap,
-  getRotation,
-  getRotationDegree,
-  getRotationStyle,
-} from '@/src/shared/utils'
+import { colorMap, fontMap, getRotationDegree, getRotationStyle } from '@/src/shared/utils'
 import Image from 'next/image'
 
 interface SeasonWidgetProps {
@@ -23,61 +17,86 @@ interface SeasonWidgetProps {
 export const SeasonWidget = ({ topic, description, articles }: SeasonWidgetProps) => {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <div className="bg-white h-fit w-full text-center text-black font-bold p-4 text-2xl sm:text-3xl md:text-4xl lg:text-5xl -rotate-2 shadow-xl z-10">
+      <div
+        className="bg-white h-fit w-full text-center 
+      text-black font-bold p-4 text-2xl sm:text-3xl 
+      md:text-4xl lg:text-5xl -rotate-2 shadow-xl z-10"
+      >
         <h2>{topic.content}</h2>
       </div>
 
       <div className="mb-10 md:mb-15 lg:mb-20 space-y-12">
         {description.map((words, index) => (
-          <section className="flex flex-wrap gap-4 justify-center" key={words + index}>
-            {words.split(' ').map((word, index) => {
-              const randomFont = fontMap[Math.floor(Math.random() * fontMap.length)]
-              const randomBgColor = colorMap[Math.floor(Math.random() * colorMap.length)]
-              const rotationDegree = getRotationDegree()
-              const rotationStyle = getRotationStyle(rotationDegree)
-
-              return (
-                <div
-                  className={`h-fit text-black px-2 py-1 md:px-4 md:py-2 text-xl sm:texl-2xl md:text-3xl lg:text-4xl shadow-xl z-10 ${randomFont} ${randomBgColor}`}
-                  style={{ ...rotationStyle }}
-                  key={word + index}
-                >
-                  <p>{word}</p>
-                </div>
-              )
-            })}
-          </section>
+          <SeasonDescription words={words} key={words + index} />
         ))}
       </div>
 
       <article className="flex flex-wrap justify-center gap-16">
-        {articles.map((article, index) => {
-          const rotationDegree = getRotationDegree()
-          const rotationStyle = getRotationStyle(rotationDegree)
-
-          return (
-            <section
-              className={`bg-white p-3 sm:p-4 w-full sm:w-72 md:w-80 flex flex-col gap-3 hover:scale-105 transition-all duration-300 shadow-xl cursor-pointer`}
-              key={article.title + index}
-              style={{ ...rotationStyle }}
-            >
-              <div className="bg-black aspect-square w-full relative overflow-hidden">
-                {article.imageSrc ? (
-                  <Image src={article.imageSrc} alt={article.title} fill className="object-cover" />
-                ) : (
-                  <div className="absolute inset-0 from-gray-800 to-black" />
-                )}
-              </div>
-              <p className="text-black text-xl sm:text-2xl md:text-3xl font-bold">
-                {article.title}
-              </p>
-              <span className="text-gray-700 text-sm sm:text-base line-clamp-4">
-                {article.description}
-              </span>
-            </section>
-          )
-        })}
+        {articles.map((article, index) => (
+          <SeasonArticle article={article} key={article.title + index} />
+        ))}
       </article>
+    </div>
+  )
+}
+
+const SeasonDescription = ({ words }: { words: string }) => {
+  return (
+    <section className="flex flex-wrap gap-4 justify-center">
+      {words.split(' ').map((word, index) => {
+        const randomFont = fontMap[Math.floor(Math.random() * fontMap.length)]
+        const randomBgColor = colorMap[Math.floor(Math.random() * colorMap.length)]
+        const rotationDegree = getRotationDegree()
+        const rotationStyle = getRotationStyle(rotationDegree)
+
+        return (
+          <div
+            className={`h-fit text-black px-2 py-1 md:px-4 md:py-2 
+                    text-xl sm:texl-2xl md:text-3xl lg:text-4xl 
+                    shadow-xl z-10 ${randomFont} ${randomBgColor}`}
+            style={{ ...rotationStyle }}
+            key={word + index}
+          >
+            <p>{word}</p>
+          </div>
+        )
+      })}
+    </section>
+  )
+}
+
+const SeasonArticle = ({
+  article,
+}: {
+  article: {
+    title: string
+    description: string
+    imageSrc: string
+  }
+}) => {
+  return (
+    <div className="w-fit space-y-4">
+      <section
+        className={`bg-white p-3 sm:p-4 flex flex-col gap-3 
+                  hover:scale-105 transition-all duration-300 shadow-xl cursor-pointer`}
+      >
+        <div className="bg-black aspect-square w-full relative overflow-hidden">
+          {article.imageSrc ? (
+            <Image src={article.imageSrc} alt={article.title} fill className="object-cover" />
+          ) : (
+            <div className="absolute inset-0 from-gray-800 to-black" />
+          )}
+        </div>
+        <p className="text-black text-xl sm:text-2xl md:text-3xl font-bold whitespace-nowrap">
+          {article.title}
+        </p>
+      </section>
+      <div
+        className={`bg-white text-gray-700 text-sm sm:text-base px-2 py-1 
+                  md:px-4 md:py-2 hover:scale-105 transition-all duration-300 max-w-100 cursor-pointer`}
+      >
+        <p className="line-clamp-3">{article.description}</p>
+      </div>
     </div>
   )
 }
